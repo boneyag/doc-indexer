dbe46de25112eeb033d983bbc8b632cdc1e546c8
akalankag <boney.ag@gmail.com>
2024-11-26 10:31:27 -0700

    remove dummy soup test file from the repo

--- src/indexer/soup_test.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import os
-
-
-with open(os.path.join(os.path.dirname(__file__), '../data/pandas/pandas.core.groupby.DataFrameGroupBy.std.html'), 'r') as f:
-    html_content = f.read()
-soup = BeautifulSoup(html_content, 'html.parser')
-print(soup.h1)
